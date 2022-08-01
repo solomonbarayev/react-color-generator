9c55b2c1ee12206cc4015aa9ecd885f14fecf737
solomonbarayev <sbarayev2@gmail.com>
2022-08-01 12:11:27 +0300

    Updates

---
+++ precache-manifest.9c4aeb863344e6f94a3cacabb293bc6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5defc83ba0dbc037388bec2463fe7fb",
+    "url": "/react-color-generator/index.html"
+  },
+  {
+    "revision": "a0af70df6025e80462f1",
+    "url": "/react-color-generator/static/css/main.9f3b6256.chunk.css"
+  },
+  {
+    "revision": "ad4df7224736efd1468d",
+    "url": "/react-color-generator/static/js/2.db4accaa.chunk.js"
+  },
+  {
+    "revision": "85e50a0285581219fe721d1b9966173b",
+    "url": "/react-color-generator/static/js/2.db4accaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0af70df6025e80462f1",
+    "url": "/react-color-generator/static/js/main.07e56a30.chunk.js"
+  },
+  {
+    "revision": "86272262674149d27e8a",
+    "url": "/react-color-generator/static/js/runtime-main.9c11df16.js"
+  }
+]);
